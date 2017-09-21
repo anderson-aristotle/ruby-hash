4a0b550e920dd44529938ba5dcd6a5a8ba173eb2
jcornmanhomonoff <jcornmanhomonoff@gmail.com>
2017-09-21 13:55:03 -0400

    Fix linter errors
    Add `_` to ignore unused variables.
    Add blank line after magic comments.
    Use `%i` for array of symbols.

--- snippets/hash_elements.rb
+++ snippets/hash_elements.rb
@@ -3,14 +3,15 @@
 priced_apartment[:occupants] = []
 # => []
 
-lee = {name: 'Lee', age: 24, dog: 'Fluffy'}
+lee = { name: 'Lee', age: 24, dog: 'Fluffy' }
 # => {:name=>"Lee", :age=>24, :dog=>"Fluffy"}
 
-adrian = {name: 'Adrian', age: 25, cat: 'Scratchy'}
+adrian = { name: 'Adrian', age: 25, cat: 'Scratchy' }
 # => {:name=>"Adrian", :age=>25, :cat=>"Scratchy"}
 
 priced_apartment[:occupants].push(lee, adrian)
-# => [{:name=>"Lee", :age=>24, :dog=>"Fluffy"}, {:name=>"Adrian", :age=>25, :cat=>"Scratchy"}]
+# => [{:name=>"Lee", :age=>24, :dog=>"Fluffy"},
+# {:name=>"Adrian", :age=>25, :cat=>"Scratchy"}]
 
 priced_apartment[:occupants][1].delete(:cat)
 # => "Scratchy"
