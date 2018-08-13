7574c576bf43eef5ec3f661952d71b368d7afb9f
tvlangley <chibia2@gmail.com>
2018-08-13 11:29:51 -0400

    Update 026/master from ruby-template
    Refresh for cohort 026

--- snippets/hash_keys.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-priced_apartment.keys
-# => [:address, :bedrooms, :occupants, :rent]
--- snippets/terminal_hash_argument.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-[].push 4, 'hi', first_name: 'dave', last_name: 'smith'
-# => [4, "hi", {:first_name=>"dave", :last_name=>"smith"}]
